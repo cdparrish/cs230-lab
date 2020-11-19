b6fe1225103d5e1c765d0d37d848f10ed920e9d7
carterparrish <carterparri@gmail.com>
2020-11-18 20:31:50 -0500

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -21,7 +21,7 @@ session_start();
     </head>
     <header>
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark" style="">
-            <a class="navbar-brand" href="/cs230-lab/gallery.php">Milky Market</a>
+            <a class="navbar-brand" href="gallery.php">Milky Market</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
@@ -31,17 +31,17 @@ session_start();
                         <a class="nav-link" href="#">Deals</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="/cs230-lab/index.php">About</a>
+                        <a class="nav-link" href="index.php">About</a>
                     </li>
                     <?php
                     if(isset($_SESSION['uid'])){
                         echo '<li class="nav-item">
-                        <a class="nav-link" href="/cs230-lab/includes/logout.php">Logout</a>
+                        <a class="nav-link" href="logout.php">Logout</a>
                     </li>';
                     }
                     else{
                         echo '<li class="nav-item">
-                        <a class="nav-link" href="/cs230-lab/login.php">Login</a>
+                        <a class="nav-link" href="login.php">Login</a>
                     </li>';
                     }
                     ?>

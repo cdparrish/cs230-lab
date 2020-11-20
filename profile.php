8613d7127873eb44f914db7adc7a4a9254169d3c
carterparrish <carterparri@gmail.com>
2020-11-19 19:13:19 -0500

    Update profile.php

--- profile.php
+++ profile.php
@@ -15,12 +15,6 @@ require 'includes/dbhandler.php'
         $photo = $row['picpath'];
     ?>
     <style>
-        .bg-cover{
-            background-image: url("/images/space-bg.jpg");
-            height: 100vh;
-            background-position: center;
-            background-size: cover;
-        }
         .center-me {
             display: flex;
             justify-content: center;
@@ -59,8 +53,6 @@ require 'includes/dbhandler.php'
             }
         }
     </script>
-
-        <div class="bg-cover">
     <div class="h-100 center-me text-center">
         <div class="my-auto">
             <form action="/includes/upload-helper.php" method="POST" enctype="multipart/form-data">
@@ -80,7 +72,6 @@ require 'includes/dbhandler.php'
             </form>
         </div>
     </div>
-        </div>
     <?php
     }else{
         header("Location: login.php");
